8e3f1472ed44ef7148dd1798bfc9784662e1654c
twisti <denizgebauer@web.de>
2025-06-28 21:36:12 +0200

    Add block break event handling to drop inventory contents

--- surf-radnom-drops-event/src/main/kotlin/dev/slne/surf/event/randomdrops/listener/random/RandomBlockDropListener.kt
+++ surf-radnom-drops-event/src/main/kotlin/dev/slne/surf/event/randomdrops/listener/random/RandomBlockDropListener.kt
@@ -1,9 +1,12 @@
 package dev.slne.surf.event.randomdrops.listener.random
 
 import dev.slne.surf.event.randomdrops.service.PlayerDropService
+import org.bukkit.block.Container
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockDropItemEvent
+import org.bukkit.inventory.InventoryHolder
 
 object RandomBlockDropListener : Listener {
     @EventHandler
@@ -16,6 +19,21 @@ object RandomBlockDropListener : Listener {
             val originalType = dropItem.type.asItemType() ?: return
             val replacedType = PlayerDropService.getReplacedBlockDrop(uuid, originalType)
             drop.itemStack = replacedType.createItemStack(dropItem.amount)
+        }
+    }
+
+    @EventHandler
+    fun onBlockBreak(event: BlockBreakEvent) {
+        val block = event.block.state
+        if (block is InventoryHolder) {
+            val inventory = block.inventory
+            val content = inventory.contents
+            inventory.clear()
+
+            for (content in content) {
+                if (content == null) continue
+                block.world.dropItemNaturally(block.location, content)
+            }
         }
     }
 }
