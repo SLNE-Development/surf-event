e5911274c3d7af64b3d53e6fbf4236cd6c3a2a4e
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-28 23:57:04 +0200

    feat: simplify corpse death listener by removing unnecessary game mode checks

--- surf-event-events/surf-anarchy/src/main/kotlin/dev/slne/surf/event/anarchy/corpse/CorpseDeathListener.kt
+++ surf-event-events/surf-anarchy/src/main/kotlin/dev/slne/surf/event/anarchy/corpse/CorpseDeathListener.kt
@@ -1,26 +1,14 @@
 package dev.slne.surf.event.anarchy.corpse
 
-import dev.slne.surf.event.anarchy.spectating.SpectatingManager
-import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
 
 object CorpseDeathListener : Listener {
-    private val spectatingGamemodes = listOf(GameMode.CREATIVE, GameMode.SPECTATOR)
-
     @EventHandler(priority = EventPriority.LOW)
     fun onPlayerDeath(event: PlayerDeathEvent) {
         val player = event.entity
-
-        if (player.gameMode in spectatingGamemodes) {
-            return
-        }
-
-        if (SpectatingManager.isSpectator(player)) {
-            return
-        }
 
         val contents = player.inventory.contents.clone()
 
